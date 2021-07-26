09f810d006d4cc78915a3b219973f198f1fd0a09
Gürkan <gitoperations@gurkan.in>
2021-07-26 23:04:15 +0200

    Switch to substitute module for scripts

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -9,10 +9,4 @@ in
     hashKnownHosts = true;
     matchBlocks = secrets.sshMatchBlocks;
   };
-
-  home.file.".ssh/.folder-README".text = ''
-    config: Comes from home-manager
-    keys:   Comes from syncthing (with exception of *neversync* regex for keys I don't want to sync)
-    others: Local files
-  '';
 }
