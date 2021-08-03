bc7fd201234a1451c3986fef3288691790b781b5
Gürkan Gür <gurkan.gur@innogames.com>
2021-08-03 11:36:25 +0200

    Comments on ff user.js

--- modules/config_files/firefox/user.js
+++ modules/config_files/firefox/user.js
@@ -1,3 +1,4 @@
+/* Privacy stuff, it's a damn shame that we need to do it this way ***/
 user_pref("app.normandy.first_run", false);
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr", false);
@@ -7,22 +8,30 @@ user_pref("browser.newtabpage.activity-stream.disableSnippets", true);
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
 user_pref("browser.pocket.enabled", false);
-user_pref("browser.tabs.warnOnClose", false);
-user_pref("browser.download.useDownloadDir", false);
-user_pref("browser.urlbar.clickSelectsAll", true);
-user_pref("browser.urlbar.doubleClickSelectsAll", false);
-user_pref("dom.webnotifications.enabled", false);
 user_pref("extensions.pocket.enabled", false);
-user_pref("general.warnOnAboutConfig", false);
-user_pref("security.webauth.u2f", true);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.tabs.closeWindowWithLastTab", false);
-user_pref("browser.cache.disk.smart_size.enabled", false);
-user_pref("browser.cache.disk.capacity", 500000);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
-user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);
+
+/* Cache less ***/
 user_pref("browser.cache.disk.capacity", 100000);
+user_pref("browser.cache.disk.smart_size.enabled", false);
+
+/* Feature stuff ***/
 user_pref("dom.w3c_touch_events.enabled", 1);
+user_pref("security.webauth.u2f", true);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("dom.webnotifications.enabled", false);
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.urlbar.clickSelectsAll", true);
+user_pref("browser.urlbar.doubleClickSelectsAll", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+/* Do not have "offline mode" for fucks sake, we're not on 1992 ***/
 user_pref("network.manage-offline-status", false);
+
+/* Helps on hidpi ***/
+user_pref("layout.css.devPixelsPerPx", 1.2);
