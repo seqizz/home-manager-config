2ae083da118eda62bc4f235a285f8128aa80c506
Gürkan Gür <gurkan.gur@innogames.com>
2021-08-03 11:36:04 +0200

    Switch paoutput to forked version

--- modules/packages/paoutput.nix
+++ modules/packages/paoutput.nix
@@ -1,13 +1,12 @@
-{ stdenv, fetchFromGitHub, pulseaudioFull }:
+{ stdenv, fetchgit , pulseaudioFull }:
 
 stdenv.mkDerivation {
   name = "paoutput";
   version = "0.1";
 
-  src = fetchFromGitHub {
-    owner = "davjgardner";
-    repo = "paoutput";
-    rev = "223a6af";
+  src = fetchgit {
+    url = "https://git.gurkan.in/gurkan/paoutput.git";
+    rev = "223a6af7b9f1bad06f53f706807d2b709dba89d9";
     sha256 = "1f1cmb9wchxr3g9hdswzlzr11wbqy58cqz56dryp8wpw3brirb02";
   };
 
