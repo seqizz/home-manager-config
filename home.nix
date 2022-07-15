4024dea7666cf9c4ede13a28fa510425aa07a410
Gürkan <gitoperations@gurkan.in>
2022-07-15 16:38:41 +0200

    Move sheldon to main config

--- home.nix
+++ home.nix
@@ -16,20 +16,6 @@ with import <home-manager/modules/lib/dag.nix> { inherit lib; };
     find ~ -name "*.FUCK" -print -delete
   '';
 
-  # XXX: Managing plugins via sheldon, since nix-way is basically
-  # "Oh let's write the hashsums of every f-king plugin's every version we use"
-  home.activation.updateSheldon = dagEntryAfter ["writeBoundary"] ''
-    #!/usr/bin/env zsh
-
-    if [ ! -e /tmp/.sheldon_updated ] || [ `stat --format=%Y /tmp/.sheldon_updated` -le $(( `date +%s` - 86400 )) ]; then
-      echo "Updating sheldon plugins.."
-      sheldon lock --update || echo "Had problems, ignoring!"
-      touch /tmp/.sheldon_updated
-    else
-      echo "Skipping sheldon update, recently done"
-    fi
-  '';
-
   programs.home-manager = {
     enable = true;
   };
